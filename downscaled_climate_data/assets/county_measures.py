10fff7c24a4814cd4dcc224d7038d8615537009a
Ben Galewsky <ben@peartreestudio.net>
2025-05-29 11:19:37 -0500

    Try adding dask

--- downscaled_climate_data/assets/county_measures.py
+++ downscaled_climate_data/assets/county_measures.py
@@ -5,12 +5,30 @@ from shapely.geometry import Point
 from dask.distributed import Client
 
 
+from htcdaskgateway import HTCGateway
+from dask_gateway.auth import BasicAuth
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+os.environ['CONDOR_BIN_DIR'] = "/opt/conda/envs/pangeo/bin"
+gateway = HTCGateway(address="https://dask.software-dev.ncsa.illinois.edu",
+                     proxy_address=8786,
+                     auth = BasicAuth(
+                         username=None, 
+                         password=os.environ['DASK_GATEWAY_PASSWORD'])
+                    )
+cluster = gateway.new_cluster(image="bengal1/pangeo-ncsa:dev", 
+                              container_image="/u/bengal1/condor/pangeo.sif")
+cluster.scale(200)
+client = cluster.get_client()
+print(client)
+
 t2m = era5_processing(['2m_temperature', 'total_precipitation'], 2024, 2025, 'analysis_ready')
 df = t2m.to_dataframe()
 
 print(df.head())
 
-# Get the current Dask client and shut it down
-client = Client.current()
 if client is not None:
     client.close()
